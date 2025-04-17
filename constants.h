f00083275eeb798154431deed468d89df8b0df20
Pablo <pableroncio.suarez@gmail.com>
2025-04-17 21:22:45 +0200

    mejoradas las colisiones

--- constants.h
+++ constants.h
@@ -1,11 +1,9 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int RADIOUS = 10;
 const int WIDTH = 1300;
 const int HEIGTH = 900;
-const int Frame_rate = 200;
-const float RESTITUTION = 0.4f;
-const float MASS = 1;
-const float MPI = 3;
+const int Frame_rate = 300;
+const float BOX_RESTITUTION = 1.0f;
+const float PART_RESTITUTION = 0.8f;
 #endif
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 #include "particle.h"
 #include "constants.h"
 
@@ -15,22 +16,65 @@ sf::Color getRandomColor() {
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({WIDTH, HEIGTH}), "SFML works!");
+    //random init
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(-1, 1);
+    std::uniform_int_distribution<> distrib2(1, WIDTH);
+    std::uniform_int_distribution<> distrib3(1, HEIGTH);
 
+    //Create Window
+    sf::RenderWindow window(sf::VideoMode({WIDTH, HEIGTH}), "SFML works!");
     window.setFramerateLimit(Frame_rate);
+
+    //Create particles
     std::vector<Particle> particles;
-    particles.emplace_back(1,WIDTH/2,RADIOUS,false,10,sf::Vector2f(0,500));
-    particles.emplace_back(2,WIDTH/2,(HEIGTH-20),false,40,sf::Vector2f(0,-300));
-    int i = 0;
+
+    
+    for (int i = 0; i<1000; i++){
+        
+        float vel_x = distrib(gen);
+        float vel_y = distrib(gen);
+        
+        float rand_width = distrib2(gen);
+        
+        float rand_height = distrib3(gen);
+        particles.emplace_back(1,rand_width,rand_height,false,13,sf::Vector2f(vel_x, vel_y));
+    }
+
+    bool can_press = true;
     while (window.isOpen())
     {
+        
+        if (!sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        {
+            can_press = true;
+        }else if (can_press && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)){
+            can_press = false;
+            sf::Vector2i localPosition = sf::Mouse::getPosition(window); 
+            particles.emplace_back(1,localPosition.x,localPosition.y,false,13,sf::Vector2f(0,0));
+        }
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-        i++;
+        
         window.clear();
+
+        
+        for (int i = 0; i< particles.size(); i++){
+            particles[i].applyForce({0,0.1f*particles[i].mass});
+            
+            particles[i].EdgeCollisions();
+            for (int j = i+1; j< particles.size();j++){
+                particles[i].ParticleCollision(particles[j]);
+                
+                
+            }
+            particles[i].update();
+        }
+        
 
         for (const auto& particle: particles) {
             sf::CircleShape circle(particle.radius);
@@ -40,23 +84,9 @@ int main()
             window.draw(circle);
         }
         
-        for (size_t i = 0; i < particles.size(); ++i) {
-            particles[i].update(1/ (float) Frame_rate);
-            for (size_t j = i + 1; j < particles.size(); ++j) {
-                particles[i].ParticleCollision(particles[j]);
-            }
-        }
-        
-        // Actualizar velocidades si usas next_velocity
-        for (auto& particle : particles) {
-            particle.updateVelocity();
-        }
-        
-        // Dibujar (una sola vez)
        
-
-        if (i%(Frame_rate / 5) == 0)
-        particles.emplace_back(3,RADIOUS,5,false,10,sf::Vector2f(800,100));
+        
+        
 
         window.display();
     }
